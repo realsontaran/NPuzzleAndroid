bb9bd83f16cef24e28a773c81b128b39befd1792
realsontaran <e.ozbay75@gmail.com>
2020-01-31 14:04:49 +0300

    EditText to Spinner

--- app/src/main/java/com/example/npuzzle/MainActivity.java
+++ app/src/main/java/com/example/npuzzle/MainActivity.java
@@ -4,37 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.LogPrinter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    EditText editText;
-    TextView textView;
+    Board puzzle;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spnCol);
+        String tmp = spinner.getSelectedItem().toString();
+        int col = Integer.parseInt(tmp);
+        spinner = findViewById(R.id.spnRow);
+        tmp = spinner.getSelectedItem().toString();
+        int row = Integer.parseInt(tmp);
 
-
-        Button create = findViewById(R.id.createButton);
-
-        create.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText = findViewById(R.id.colText);
-                textView = findViewById(R.id.colView);
-                textView.setText("col = "+editText.getText().toString());
-                editText = findViewById(R.id.rowText);
-                textView = findViewById(R.id.rowView);
-                textView.setText("row = "+editText.getText().toString());
-            }
-        });
+        puzzle = new Board(col,row);
 
     }
 }
